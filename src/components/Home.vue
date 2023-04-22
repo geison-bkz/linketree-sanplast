2a677634dcefc1cc037726dcf64798150d702dbb
geisondossantos <geison_bf@hotmail.com>
2023-04-22 15:55:49 -0300

    home concluida

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <body>
     <header>
-      <div class="share-button"></div>
     </header>
     <div class="container">
       <div class="image-container">
@@ -12,32 +11,84 @@
       </div>
       <h1>@sanplastindustrialtda</h1>
 
-      <a class="tile" href="">
+      <a class="tile" href="http://sanplast.ind.br/produtos">
         <div class="icon">
-          <font-awesome-icon :icon="['fas', 'house']" size="lg"/>
+          <font-awesome-icon :icon="['fas', 'house']" size="lg" />
         </div>
         <p>Sanplast</p>
-        <div class="tile-share-button"></div>
+        <div class="tile-share-button" link="http://sanplast.ind.br/produtos">
+          <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" />
+        </div>
       </a>
-      <a class="tile" href="">
+      <a
+        class="tile"
+        href="https://www.linkedin.com/company/sanplast-industria-de-plasticos-ltda/"
+      >
         <div class="icon">
           <font-awesome-icon :icon="['fab', 'linkedin']" size="xl" />
         </div>
         <p>Linkedin</p>
-        <div class="tile-share-button"></div>
+        <div
+          class="tile-share-button"
+          link="https://www.linkedin.com/company/sanplast-industria-de-plasticos-ltda/"
+        >
+          <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" />
+        </div>
       </a>
-      <a class="tile" href="">
+      <a
+        class="tile"
+        href="https://www.facebook.com/sanplastindustriadeplastico/"
+      >
         <div class="icon">
-          <font-awesome-icon :icon="['fab', 'facebook']" size="xl"/>
+          <font-awesome-icon :icon="['fab', 'facebook']" size="xl" />
         </div>
         <p>Facebook</p>
-        <div class="tile-share-button"></div>
+        <div
+          class="tile-share-button"
+          link="https://www.facebook.com/sanplastindustriadeplastico/"
+        >
+          <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" />
+        </div>
+      </a>
+
+      <a
+        class="tile"
+        href="https://api.whatsapp.com/send/?phone=5548996297511&text&type=phone_number&app_absent=0"
+      >
+        <div class="icon">
+          <font-awesome-icon :icon="['fab', 'whatsapp']" size="xl"/>
+        </div>
+        <p>Whatsapp</p>
+        <div
+          class="tile-share-button"
+          link="https://api.whatsapp.com/send/?phone=5548996297511&text&type=phone_number&app_absent=0"
+        >
+          <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" />
+        </div>
       </a>
     </div>
   </body>
 </template>
 
-<script setup></script>
+<script setup>
+const shareButtons = document.querySelectorAll('.tile-share-button');
+
+async function copyText(e) {
+  e.preventDefault();
+  const link = this.getAttribute('link');
+
+  try {
+    await navigator.clipboard.writeText(link);
+    alert('Texto copiado: ' + link);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+shareButtons.forEach((shareButton) =>
+  shareButton.addEventListener('click', copyText)
+);
+</script>
 
 <style>
 body {
@@ -49,15 +100,19 @@ body {
   align-items: center;
   flex-direction: column;
   width: 100vw;
+  background-image: url("../assets/images/sanplast-bg-visao.jpg");
+  background-size: auto;
+  background-repeat: no-repeat;
+  background-position: left top;
 }
 
 header {
-  width: 95%;
-  max-width: 788px;
+  width: 100%;
+  max-width: 100%;
   display: flex;
   justify-content: flex-end;
-  padding: 12px;
-  margin-top: 15px;
+  padding: 20px;
+  background-color: white;
 }
 
 .share-button {
@@ -65,6 +120,12 @@ header {
   height: 40px;
   border-radius: 20px;
   background-color: black;
+}
+
+.share-button svg {
+  margin-left: 13px;
+  margin-top: 12px;
+  color: white;
 }
 
 .container {
@@ -79,6 +140,7 @@ header {
 h1 {
   font-size: 20px;
   margin-bottom: 30px;
+  color: white;
 }
 
 a {
@@ -96,12 +158,22 @@ a {
   justify-content: space-between;
 }
 
+.tile:hover {
+  transition: cubic-bezier(0.07, 1.41, 0.82, 1.41) 0.2s;
+  transform: scale(1.02);
+}
+
 .tile-share-button {
   margin: 8px;
   width: 40px;
   height: 40px;
   border-radius: 20px;
   background-color: rgb(52, 52, 52);
+}
+
+.tile-share-button svg {
+  margin-left: 13px;
+  margin-top: 12px;
 }
 
 .image-container {
--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import './style.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons';
-library.add(faLinkedin, faHome, faFacebook);
+import { faHome, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+library.add(faLinkedin, faHome, faFacebook, faArrowRightFromBracket, faWhatsapp);
 
 createApp(App).use(router).use(store).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
